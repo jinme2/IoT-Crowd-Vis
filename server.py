5266af74eb4271c9c546a9ec834601e333bfc048
sanhae <zxnm2245@naver.com>
2025-11-23 21:20:42 +0900

    CORS all open for testing

--- server.py
+++ server.py
@@ -3,8 +3,10 @@ import os
 import csv
 import pymysql
 from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # ======================================
 # 📌 MySQL 연결
